f6bc9434dad5cae0460f12bebb0af1bbd4814a9e
Crowdbotics <team@crowdbotics.com>
2021-03-11 14:45:37 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/red_credit_dev_20424/MainActivity.java
@@ -0,0 +1,15 @@
+package com.red_credit_dev_20424;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "red_credit_dev_20424";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://red-credit-dev-20424.botics.co",
+  defaultTimeout: 5000
+};
